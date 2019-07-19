07b110489ded70dff2d7a4933fa09a2a8ec3c4c5
Sean Warren <sean@scape.io>
2019-07-19 10:11:31 +0100

    Update eigen version number

--- pkgs/development/libraries/eigen/default.nix
+++ pkgs/development/libraries/eigen/default.nix
@@ -1,14 +1,14 @@
 {stdenv, fetchurl, cmake}:
 
 let
-  version = "3.3.7a";
+  version = "3.3.8";
 in
 stdenv.mkDerivation {
   name = "eigen-${version}";
 
   src = fetchurl {
     url = "https://bitbucket.org/eigen/eigen/get/7577527c33a6c4df8a321ea50378285cae83c3dd.tar.gz";
-    name = "eigen-${version}.tar.gz";
+    name = "eigen-eigen-7577527c33a6.tar.gz";
     sha256 = "1f7a5zvcx5iway1gp9s3qk1yb3jrkam2lr1cqwd3jq3s3lldsp7p";
   };
 
