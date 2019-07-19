722578266f30d887d543c1a78c00ab7785068d88
Sean Warren <sean@scape.io>
2019-07-19 10:07:08 +0100

    Update eigen to current head

--- pkgs/development/libraries/eigen/default.nix
+++ pkgs/development/libraries/eigen/default.nix
@@ -1,15 +1,15 @@
 {stdenv, fetchurl, cmake}:
 
 let
-  version = "3.3.7";
+  version = "3.3.7a";
 in
 stdenv.mkDerivation {
   name = "eigen-${version}";
 
   src = fetchurl {
-    url = "https://bitbucket.org/eigen/eigen/get/${version}.tar.gz";
+    url = "https://bitbucket.org/eigen/eigen/get/7577527c33a6c4df8a321ea50378285cae83c3dd.tar.gz";
     name = "eigen-${version}.tar.gz";
-    sha256 = "1nnh0v82a5xibcjaph51mx06mxbllk77fvihnd5ba0kpl23yz13y";
+    sha256 = "1f7a5zvcx5iway1gp9s3qk1yb3jrkam2lr1cqwd3jq3s3lldsp7p";
   };
 
   patches = [
